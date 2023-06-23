d4d9e65ba5dc94d9319cd4efe019d26f3e55c46b
Robin <robin.t@live.be>
2023-06-23 19:25:05 +0200

    modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,51 @@
-let emYears = document.getElementById('years')
-let emMonths = document.getElementById('months')
-let emDay = document.getElementById('days')
+// input user's values
+let inputD = document.getElementById('day')
+let inputM = document.getElementById('month')
+let inputY = document.getElementById('year')
 
-let inputYear = document.getElementById('year')
-let inputMonth = document.getElementById('month')
-let inputDay = document.getElementById('day')
+// em result of user's values
+let emD = document.getElementById('days')
+let emM = document.getElementById('months')
+let emY = document.getElementById('years')
 
-function getAmountOfLeapYears(year) {
-	const date = new Date()
-	let sum = 0
-	for (let i = year; i < date.getFullYear(); i++) {
-		if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) sum++
-	}
-	return sum
+/**
+ * 
+ * @param {string} birthDay
+ */
+function getAge(bday, bmonth, byear) {
+   const date = new Date(),
+   day = date.getDate(),
+   month = date.getMonth() + 1,
+   year = date.getFullYear()
+
+   if (day < bday) {
+    emD.textContent = (day - bday + 30)
+    month--
+   } else {
+    emD.textContent = (day - bday)
+   }
+
+   if (month < bmonth) {
+    emM.textContent = (month - bmonth + 12)
+    year--
+   } else {
+    emM.textContent = (month - bmonth)
+   }
+
+   emY.textContent = year - byear
 }
 
-function getAge(birthDay) {
-	const date = new Date()
-	let birthDate = new Date(birthDay)
-	
-	let tempYear,
-		tempMonth,
-		tempDay,
-		temp
-
-	let amountOfLeapYears = getAmountOfLeapYears(year)
-
-	let diff = date.getTime() - birthDate.getTime()
-	temp = Math.trunc(diff / (1000 * 3600 * 24)) + amountOfLeapYears
-
-	tempYear = temp
-	
-	/*
-	tempDay = (temp % 365) % 7
-	tempMonth = Math.trunc(temp / 30.436806)
-	tempYear = Math.trunc(temp / 365)
-	
-	/* END -------------------- */
-	
-	emYears.textContent = tempYear
-	emMonths.textContent = tempMonth
-	emDay.textContent = tempDay
+if (window.innerWidth >= 1440) {
+   emD.textContent = "--"
+   emM.textContent = "--"
+   emY.textContent = "--"
 }
 
-function average(arr) {
-	let sum = 0
-	for (let i = 0; i < arr.length; i++) {
-		sum += arr[i]
-	}
-	return sum / arr.length
-}
-
-function getDayOfMonth(monthNum) {
-	// if april, june, september or november return 30
-	if (monthNum == 4 || 
-		monthNum == 6 ||
-		monthNum == 9 ||
-		monthNum == 11) return 30
-	// if february return 28
-	else if (monthNum == 2) return 28
-	// else return 30
-	return 30
-}
-
-let day,
-	month,
-	year
-
-document.addEventListener('keypress', (e) => {
-	if (e.code == "Enter") {
-		day = inputDay.value
-		month = inputMonth.value
-		year = inputYear.value
-		emDay.textContent = getAge(month + "/" + day + "/" + year)
-	}
-})
+/**
+ * event listener on key press check if key is {Enter}
+ */
+document.addEventListener('keypress', (event) => {
+    if (event.code == "Enter") {
+        getAge(inputD.value, inputM.value, inputY.value)
+    }
+})
